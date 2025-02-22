d8d93bbbe3b2ea88732c797dc8e500b67e506fa3
Ariful Islam <156695945+Ariful-Islam010@users.noreply.github.com>
2025-02-22 19:46:22 +0600

    Update Counting Sort.c

--- Counting Sort.c
+++ Counting Sort.c
@@ -1,23 +1,21 @@
-#include<stdio.h>
-int main()
-{
+#include<iostream>
+using namespace std;
+
+int main() {
     int arr[1000], n, i;
 
-    printf("Enter the size of the array: ");
-    scanf("%d", &n);
+    cout << "Enter the size of the array: ";
+    cin >> n;
 
-    printf("Enter the elements of the array: ");
-    for(i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
+    cout << "Enter the elements of the array: ";
+    for(i = 0; i < n; i++) {
+        cin >> arr[i];
     }
 
     // Find the maximum element in the array
-    int max = arr[0];//Range and max are exactly the same
-    for(i = 1; i < n; i++)
-    {
-        if(arr[i] > max)
-        {
+    int max = arr[0]; // Range and max are exactly the same
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max) {
             max = arr[i];
         }
     }
@@ -28,28 +26,23 @@ int main()
         count[i] = 0;
     }
 
-
     // Store the count of each element in the count array.
     for (i = 0; i < n; i++) {
         count[arr[i]]++;
     }
 
-    for(i = 1; i <= max; i++)
-    {
-        count[i]+=count[i-1];
+    for(i = 1; i <= max; i++) {
+        count[i] += count[i - 1];
     }
 
     int output[n];
-    for(i=n-1; i >= 0; i--)
-    {
-        output[--count[arr[i]]] =arr[i];
-
+    for(i = n - 1; i >= 0; i--) {
+        output[--count[arr[i]]] = arr[i];
     }
 
-    printf("Sorted array in ascending order: ");
-    for(i = 0; i < n; i++)
-    {
-        printf("%d ", output[i]);
+    cout << "Sorted array in ascending order: ";
+    for(i = 0; i < n; i++) {
+        cout << output[i] << " ";
     }
 
     return 0;
